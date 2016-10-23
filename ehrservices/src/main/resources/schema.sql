e1a69f5415d08489749cd70738693ba2ebddef4d
Vivek Malhotra <vivek.malhotra@ext.automic.com>
2016-10-23 17:09:45 +0530

    schema and data sql updated

--- ehrservices/src/main/resources/schema.sql
+++ ehrservices/src/main/resources/schema.sql
@@ -3,46 +3,34 @@
 -- Table structure for table `internal_user`
 --
 
-DROP TABLE IF EXISTS `internal_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `internal_user` (
   `USER_ID` int(11) NOT NULL AUTO_INCREMENT,
   `FULL_NAME` varchar(45) NOT NULL,
   PRIMARY KEY (`USER_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
 -- Table structure for table `patient`
 --
-
-DROP TABLE IF EXISTS `patient`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `patient` (
   `PATIENT_ID` int(11) NOT NULL,
-  `FULL_NAME` varchar(45)  NOT NULL,
+  `FULL_NAME` varchar(45) NOT NULL,
   `DATE_OF_BIRTH` date NOT NULL,
-  `ADDRESS` varchar(100)  NOT NULL,
-  `BLOOD_GROUP` varchar(5)  NOT NULL,
-  `GENDER` varchar(10)  NOT NULL,
+  `ADDRESS` varchar(100) NOT NULL,
+  `BLOOD_GROUP` varchar(5) NOT NULL,
+  `GENDER` varchar(10) NOT NULL,
   `EMAIL` varchar(45) DEFAULT NULL,
   `MOBILE` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`PATIENT_ID`),
   UNIQUE KEY `PATIENT_ID_UNIQUE` (`PATIENT_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
 -- Table structure for table `user_credential`
 --
 
-DROP TABLE IF EXISTS `user_credential`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_credential` (
   `USERNAME` varchar(45) NOT NULL,
   `PASSWORD` varchar(45) NOT NULL,
@@ -51,38 +39,30 @@ CREATE TABLE `user_credential` (
   KEY `FK_USER_ID_idx` (`USER_ID`),
   CONSTRAINT `FK_USER_ID` FOREIGN KEY (`USER_ID`) REFERENCES `internal_user` (`USER_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
 -- Table structure for table `patient_appointment`
 --
-
-DROP TABLE IF EXISTS `patient_appointment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `patient_appointment` (
   `APPOINTMENT_ID` int(11) NOT NULL AUTO_INCREMENT,
   `PATIENT_ID` int(11) NOT NULL,
   `USERNAME` varchar(45) NOT NULL,
   `APPOINTMENT_DATE` datetime NOT NULL,
+  `COMMENTS` varchar(150) NOT NULL,
   PRIMARY KEY (`APPOINTMENT_ID`),
   UNIQUE KEY `APPOINTMENT_ID_UNIQUE` (`APPOINTMENT_ID`),
   KEY `FK_PATIENT_ID_idx` (`PATIENT_ID`),
   KEY `FK_APP_USERNAME_idx` (`USERNAME`),
   CONSTRAINT `FK_APP_USERNAME` FOREIGN KEY (`USERNAME`) REFERENCES `user_credential` (`USERNAME`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_PATIENT_ID` FOREIGN KEY (`PATIENT_ID`) REFERENCES `patient` (`PATIENT_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 
 --
 -- Table structure for table `user_roles`
 --
 
-DROP TABLE IF EXISTS `user_roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_roles` (
   `ROLE_ID` int(11) NOT NULL AUTO_INCREMENT,
   `USERNAME` varchar(45) DEFAULT NULL,
@@ -92,4 +72,3 @@ CREATE TABLE `user_roles` (
   KEY `FK_USERNAME_idx` (`USERNAME`),
   CONSTRAINT `FK_USERNAME` FOREIGN KEY (`USERNAME`) REFERENCES `user_credential` (`USERNAME`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
--- ehrservices/src/main/resources/test-data.sql
+++ ehrservices/src/main/resources/test-data.sql
@@ -1,53 +1,28 @@
-
---
-
 --
 -- Dumping data for table `internal_user`
 --
-
-LOCK TABLES `internal_user` WRITE;
-/*!40000 ALTER TABLE `internal_user` DISABLE KEYS */;
-INSERT INTO `internal_user` VALUES (1,'Vivek Nahata'),(2,'Manog Babar');
-/*!40000 ALTER TABLE `internal_user` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `internal_user` VALUES (1,'Vivek Nahata'),(2,'Manoj Mehra');
 
 --
 -- Dumping data for table `user_credential`
 --
 
-LOCK TABLES `user_credential` WRITE;
-/*!40000 ALTER TABLE `user_credential` DISABLE KEYS */;
-INSERT INTO `user_credential` VALUES ('admin','password',1),('manog','password',2);
-/*!40000 ALTER TABLE `user_credential` ENABLE KEYS */;
-UNLOCK TABLES;
-
+INSERT INTO `user_credential` VALUES ('vivekmalh','password',1),('manojmehra','password',2);
 
 --
 -- Dumping data for table `patient`
 --
 
-LOCK TABLES `patient` WRITE;
-/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
-INSERT INTO `patient` VALUES (1,'Vivek Malhotra','1988-01-23','plot 312, gagan apt, palam vihar, Gurgaon','B+','MALE',NULL,NULL),(2,'Varun Malhotra','1989-06-04','3, sunview apt, Dwarka, Delhi','O+','MALE',NULL,NULL),(3,'Stephene Narang','1988-05-23','m-372, Punjabi bagh, New delhi','B+','FEMALE',NULL,NULL);
-/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `patient` VALUES (1,'Vivek Malhotra','1988-01-23','plot 312, gagan apt, palam vihar, Gurgaon','B+','MALE','abc@ymail.com','9654615544'),(2,'Varun Malhotra','1989-06-04','3, sunview apt, Dwarka, Delhi','O+','MALE','varun@ymail.com','9654312312'),(3,'Stephene Narang','1988-05-23','m-372, Punjabi bagh, New delhi','B+','FEMALE','steph@gmail.com','9878776886');
 
 --
 -- Dumping data for table `patient_appointment`
 --
 
-LOCK TABLES `patient_appointment` WRITE;
-/*!40000 ALTER TABLE `patient_appointment` DISABLE KEYS */;
-INSERT INTO `patient_appointment` VALUES (1,1,'manog','2016-10-15 17:30:00'),(2,3,'manog','2016-10-16 15:30:00');
-/*!40000 ALTER TABLE `patient_appointment` ENABLE KEYS */;
-UNLOCK TABLES;
-
+INSERT INTO `patient_appointment` VALUES (1,1,'manojmehra','2016-10-15 17:30:00',''),(2,3,'manojmehra','2016-10-16 15:30:00',''),(3,1,'manojmehra','2016-04-06 15:56:00',''),(4,2,'vivekmalh','2016-03-08 17:09:00',''),(5,1,'manojmehra','2016-04-08 17:05:00',''),(6,3,'manojmehra','2016-05-07 15:03:00','hello how are you?'),(7,2,'manojmehra','2016-10-27 17:20:00','Patient has been advised to visit again after a week of prescription');
 
 --
 -- Dumping data for table `user_roles`
 --
 
-LOCK TABLES `user_roles` WRITE;
-/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
-INSERT INTO `user_roles` VALUES (1,'admin','ROLE_DOCTOR'),(2,'manog','ROLE_DOCTOR');
-/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+INSERT INTO `user_roles` VALUES (1,'vivekmalh','ROLE_DOCTOR'),(2,'manojmehra','ROLE_DOCTOR');
